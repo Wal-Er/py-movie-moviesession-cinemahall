48056d7594a0b58f1e7b28dd0ea8493793abfffa
Walery Demidko <walerydz@gmail.com>
2026-01-29 15:21:36 +0100

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -12,8 +12,8 @@ def create_movie_session(
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=Movie.objects.get(id=movie_id),
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id)
+        movie=movie_id,
+        cinema_hall=cinema_hall_id
     )
 
 
